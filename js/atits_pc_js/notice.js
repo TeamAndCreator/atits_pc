7cabe60742a2428ebbc0b0e8e249a1e332ad5e5f
hzh macbook pro <apple@hzhdeMacBook-Pro.local>
2018-07-24 09:41:36 +0800

    add notice.js harvest.js report.js

---
+++ js/atits_pc_js/notice.js
@@ -0,0 +1,227 @@
+// Form-File-Upload.js
+// ====================================================================
+// This file should not be included in your project.
+// This is just a sample how to initialize plugins or components.
+//
+// - ThemeOn.net -
+
+
+$(document).ready(function() {
+
+    // DROPZONE.JS
+    // =================================================================
+    // Require Dropzone
+    // http://www.dropzonejs.com/
+    // =================================================================
+    Dropzone.options.demoDropzone = { // The camelized version of the ID of the form element
+        // The configuration we've talked about above
+        autoProcessQueue: false,
+        //uploadMultiple: true,
+        //parallelUploads: 25,
+        //maxFiles: 25,
+
+        // The setting up of the dropzone
+        init: function() {
+            var myDropzone = this;
+            //  Here's the change from enyo's tutorial...
+            //  $("#submit-all").click(function (e) {
+            //  e.preventDefault();
+            //  e.stopPropagation();
+            //  myDropzone.processQueue();
+            //
+            //}
+            //    );
+
+        }
+
+    }
+
+
+
+    // DROPZONE.JS WITH BOOTSTRAP'S THEME
+    // =================================================================
+    // Require Dropzone
+    // http://www.dropzonejs.com/
+    // =================================================================
+    // Get the template HTML and remove it from the doumenthe template HTML and remove it from the doument
+    var previewNode = document.querySelector("#dz-template");
+    previewNode.id = "";
+    var previewTemplate = previewNode.parentNode.innerHTML;
+    previewNode.parentNode.removeChild(previewNode);
+
+    var uplodaBtn = $('#dz-upload-btn');
+    var removeBtn = $('#dz-remove-btn');
+    var myDropzone = new Dropzone(document.body, { // Make the whole body a dropzone
+        url: "/target-url", // Set the url
+        thumbnailWidth: 50,
+        thumbnailHeight: 50,
+        parallelUploads: 20,
+        previewTemplate: previewTemplate,
+        autoQueue: false, // Make sure the files aren't queued until manually added
+        previewsContainer: "#dz-previews", // Define the container to display the previews
+        clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
+    });
+
+
+    myDropzone.on("addedfile", function(file) {
+        // Hookup the button
+        uplodaBtn.prop('disabled', false);
+        removeBtn.prop('disabled', false);
+    });
+
+    // Update the total progress bar
+    myDropzone.on("totaluploadprogress", function(progress) {
+        $("#dz-total-progress .progress-bar").css({'width' : progress + "%"});
+    });
+
+    myDropzone.on("sending", function(file) {
+        // Show the total progress bar when upload starts
+        document.querySelector("#dz-total-progress").style.opacity = "1";
+    });
+
+    // Hide the total progress bar when nothing's uploading anymore
+    myDropzone.on("queuecomplete", function(progress) {
+        document.querySelector("#dz-total-progress").style.opacity = "0";
+    });
+
+
+    // Setup the buttons for all transfers
+    uplodaBtn.on('click', function() {
+        //Upload all files
+        //myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED));
+    });
+
+    removeBtn.on('click', function() {
+        myDropzone.removeAllFiles(true);
+        uplodaBtn.prop('disabled', true);
+        removeBtn.prop('disabled', true);
+    });
+
+});
+
+// Forms-Text-Editor.js
+// ====================================================================
+// This file should not be included in your project.
+// This is just a sample how to initialize plugins or components.
+//
+// - ThemeOn.net -
+
+
+$(document).ready(function() {
+
+    // SUMMERNOTE
+    // =================================================================
+    // Require Summernote
+    // http://hackerwins.github.io/summernote/
+    // =================================================================
+    $('#demo-summernote, #demo-summernote-full-width').summernote({
+        height : '230px'
+    });
+
+
+
+
+    // SUMMERNOTE AIR-MODE
+    // =================================================================
+    // Require Summernote
+    // http://hackerwins.github.io/summernote/
+    // =================================================================
+    $('#demo-summernote-airmode').summernote({
+        airMode: true
+    });
+
+
+
+
+
+    // SUMMERNOTE CLICK TO EDIT
+    // =================================================================
+    // Require Summernote
+    // http://hackerwins.github.io/summernote/
+    // =================================================================
+    $('#demo-edit-text').on('click', function(){
+        $('#demo-summernote-edit').summernote({focus: true});
+    });
+
+
+    $('#demo-save-text').on('click', function(){
+        $('#demo-summernote-edit').summernote('destroy');
+    });
+
+});
+
+
+$(document).ready(function() {
+
+    var data;
+    $.ajax({
+        crossDomain : true,
+        url:"http://47.104.26.79:8080/atits_service/activity/findAll2",
+        dataType:"json",
+        type:"get",
+        async:false,
+        success:function (result) {
+            data=result.data.activitys
+        }
+
+    });
+1
+
+
+    // EDITABLE - COMBINATION WITH X-EDITABLE
+    // =================================================================
+    // Require X-editable
+    // http://vitalets.github.io/x-editable/
+    //
+    // Require Bootstrap Table
+    // http://bootstrap-table.wenzhixin.net.cn/
+    //
+    // Require X-editable Extension of Bootstrap Table
+    // http://bootstrap-table.wenzhixin.net.cn/
+    // =================================================================
+    $('#demo-custom-toolbar').bootstrapTable({
+        idField: 'id',
+        data:data,
+        columns: [{
+            field: 'id',
+            title: 'id'
+        }, {
+            field: 'title',
+            formatter:'invoiceFormatter',
+            title: '标题',
+        }, {
+            field: 'name',
+            align: 'center',
+            title: '发布者'
+        }, {
+            field: 'date',
+            align: 'center',
+            title: '发布时间',
+        }, {
+            field: 'state',
+            align: 'center',
+            title: '状态',
+            formatter: 'statusFormatter'
+        }
+        ]
+    });
+
+});
+
+//超链接
+function invoiceFormatter(value, row) {
+    return '<a href="#" class="btn-link" >' + value + '</a>';
+}
+//状态
+function statusFormatter(value, row) {
+    var labelColor;
+    var icon = row.id % 2 === 0 ? 'fa-star' : 'fa-user';
+    if (value==1){
+        value=="通过"
+        labelColor = "success";
+    } else {
+        value=="未通过"
+        labelColor = "warning";
+    }
+    return '<div class="label label-table label-'+ labelColor+'"> ' + value + '</div>';
+}
